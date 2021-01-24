488f7325f3fdcfcf50533bb7c5c35fb42dfd2259
Frankie Wang <frankiewang19@gmail.com>
2021-01-23 20:06:53 -0500

    Finished Tutorial

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{   
+    public GameObject deathExplosion;
+    public AudioClip deathKnell;
+    public int pointValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointValue = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Die()
+    {
+        AudioSource.PlayClipAtPoint(deathKnell,gameObject.transform.position);
+        Instantiate(deathExplosion, gameObject.transform.position, Quaternion.AngleAxis(-90, Vector3.right));
+        GameObject obj = GameObject.Find("GlobalObject");
+        Global g = obj.GetComponent<Global>();
+        g.score += pointValue;
+        Destroy (gameObject);
+    }
+
+}
